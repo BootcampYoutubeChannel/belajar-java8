89dae934ec424288667fa25d5f1d36640a10b86a
dimasm93 <software.dimas_m@icloud.com>
2019-11-26 22:07:49 +0700

    membuat perulangan dengan while

---
+++ src/main/java/com/maryanto/dimas/bootcamp/loop/WhileLoop.java
@@ -0,0 +1,12 @@
+package com.maryanto.dimas.bootcamp.loop;
+
+public class WhileLoop{
+
+    public static void main(String[] args) {
+        Integer i = 0;
+        while(i < 10){
+            System.out.println("Perulangan dengan while ["+i+"]");
+            i++;
+        }
+    }
+}
