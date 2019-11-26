d8c7b02fdbddde58a3b51dc4c76b650fe5ac44c6
dimasm93 <software.dimas_m@icloud.com>
2019-11-26 22:52:38 +0700

    update if true then true statement

--- src/main/java/com/maryanto/dimas/bootcamp/selection/IfElseIf.java
+++ src/main/java/com/maryanto/dimas/bootcamp/selection/IfElseIf.java
@@ -4,7 +4,7 @@ public class IfElseIf {
     public static void main(String[] args) {
 
         String namaLengkap = "Dimas Maryanto";
-        if (namaLengkap.equals("dimas maryanto")) {
+        if (namaLengkap.equals("Dimas Maryanto")) {
             System.out.println("Data yang dibandingkan sama");
         } else if (namaLengkap.equalsIgnoreCase("dimas maryanto")) {
             System.out.println("Data yang dibandingkan beda tapi sama ketika menggunakan huruf kecil");
