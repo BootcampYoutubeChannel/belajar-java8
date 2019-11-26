dc75d0c0c5813bae9e652348c9ac9f77a0f5cfd2
dimasm93 <software.dimas_m@icloud.com>
2019-11-26 22:11:17 +0700

    membuat perulangan dengan do-while

---
+++ src/main/java/com/maryanto/dimas/bootcamp/loop/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package com.maryanto.dimas.bootcamp.loop;
+
+public class DoWhileLoop{
+
+    public static void main(String[] args) {
+        Integer i = 0;
+        do{
+            i++;
+            System.out.println("Perulangan dengan do-while ["+i+"]");
+        }while(i < 10);
+    }
+}
