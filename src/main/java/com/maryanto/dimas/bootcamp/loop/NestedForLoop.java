136bc798b57d6b02c894adb068b9836f90f5a870
dimasm93 <software.dimas_m@icloud.com>
2019-11-29 23:36:15 +0700

    membuat nested loop dengan for

---
+++ src/main/java/com/maryanto/dimas/bootcamp/loop/NestedForLoop.java
@@ -0,0 +1,14 @@
+package com.maryanto.dimas.bootcamp.loop;
+
+public class NestedForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.print(String.format("i[%d] => ", i));
+            for(int j = 0; j < 5; j++){
+                System.out.print(String.format("{%d}, ", j));
+            }
+            System.out.println();
+        }
+
+    }
+}
