7bc050824bc493e2fbcb32e78518095e43ca5c41
dimasm93 <software.dimas_m@icloud.com>
2019-11-30 00:10:01 +0700

    membuat continues statment for loop

---
+++ src/main/java/com/maryanto/dimas/bootcamp/loop/ForContinuesStatement.java
@@ -0,0 +1,10 @@
+package com.maryanto.dimas.bootcamp.loop;
+
+public class ForContinuesStatement {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++){
+            if(i == 2 || i == 8) continue;
+            System.out.println(String.format("index ke %d", i));
+        }
+    }
+}
