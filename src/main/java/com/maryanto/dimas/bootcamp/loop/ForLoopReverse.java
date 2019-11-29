500899b3639f331236a708c048e4ea946b02d62b
dimasm93 <software.dimas_m@icloud.com>
2019-11-30 00:02:51 +0700

    membuat for loop index reverse

---
+++ src/main/java/com/maryanto/dimas/bootcamp/loop/ForLoopReverse.java
@@ -0,0 +1,10 @@
+package com.maryanto.dimas.bootcamp.loop;
+
+public class ForLoopReverse {
+
+    public static void main(String[] args) {
+        for(int i = 10; i > 0; i++){
+            System.out.print(String.format("[%d] ", i));
+        }
+    }
+}
