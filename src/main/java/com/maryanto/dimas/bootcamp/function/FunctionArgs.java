5fd68f0d88b834fa83b5b35ec71c2a18484610c5
dimasm93 <software.dimas_m@icloud.com>
2019-12-02 23:22:34 +0700

    update function

--- src/main/java/com/maryanto/dimas/bootcamp/function/FunctionArgs.java
+++ src/main/java/com/maryanto/dimas/bootcamp/function/FunctionArgs.java
@@ -1,9 +1,12 @@
 package com.maryanto.dimas.bootcamp.function;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class FunctionArgs {
 
-    public static Integer getYearsOld(Integer umur){
-        return umur;
+    public static Long getYearsOld(LocalDate date){
+        return ChronoUnit.YEARS.between(date, LocalDate.now());
     }
 
     public static void setNamaLengkap(String namaLengkap){
@@ -11,13 +14,13 @@ public class FunctionArgs {
     }
 
     public static void main(String[] args) {
-        Integer umurSaya = getYearsOld(25);
+        Long umurSaya = getYearsOld(LocalDate.of(1999, 01, 10));
         System.out.println(String.format("Umur saya sekarang adalah %d", umurSaya));
 
-        umurSaya = getYearsOld(26);
+        umurSaya = getYearsOld(LocalDate.of(1998, 10, 10));
         System.out.println(String.format("Umur saya sekarang adalah %d", umurSaya));
 
-        umurSaya = getYearsOld(27);
+        umurSaya = getYearsOld(LocalDate.of(1995, 10, 10));
         System.out.println(String.format("Umur saya sekarang adalah %d", umurSaya));
 
         setNamaLengkap("Dimas Maryanto");
