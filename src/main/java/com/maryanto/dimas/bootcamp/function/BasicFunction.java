a2870532f7656742481598eba946a150b9f2292f
dimasm93 <software.dimas_m@icloud.com>
2019-12-02 22:55:47 +0700

    membuat function dan method di java

---
+++ src/main/java/com/maryanto/dimas/bootcamp/function/BasicFunction.java
@@ -0,0 +1,19 @@
+package com.maryanto.dimas.bootcamp.function;
+
+public class BasicFunction {
+
+    public static Integer getYearsOld(){
+        System.out.println("Umur saya adalah 25");
+        return 25;
+    }
+
+    public static void setNamaLengkap(){
+        System.out.println("Nama lengkap saya adalah Dimas Maryanto");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(String.format("Umur saya sekarang adalah %d", getYearsOld()));
+
+        setNamaLengkap();
+    }
+}
