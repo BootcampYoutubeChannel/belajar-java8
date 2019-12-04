af299dc64cffbd1f3ab0dd467d47f374e93e7aef
dimasm93 <software.dimas_m@icloud.com>
2019-12-04 23:41:25 +0700

    membuat polymorphism

--- src/main/java/com/maryanto/dimas/bootcamp/inheritances/Mobil.java
+++ src/main/java/com/maryanto/dimas/bootcamp/inheritances/Mobil.java
@@ -6,7 +6,15 @@ public class Mobil extends Sepeda {
 
     public String getMesin() {
         super.roda = 4;
-        return String.format("Konfigurasi mesin %s jumlah rodanya adalah %s", this.mesin, super.roda);
+        return String.format("Konfigurasi mesin %s jumlah rodanya adalah %s maximum kecepatannya adalah %s", this.mesin, super.roda, this.getKecepatan(20l));
+    }
+
+    public Long getKecepatan(){
+        return 250l;
+    }
+
+    public Long getKecepatan(Long tambahSpeed){
+        return this.getKecepatan() + tambahSpeed;
     }
 
 }
--- src/main/java/com/maryanto/dimas/bootcamp/inheritances/Sepeda.java
+++ src/main/java/com/maryanto/dimas/bootcamp/inheritances/Sepeda.java
@@ -3,4 +3,14 @@ package com.maryanto.dimas.bootcamp.inheritances;
 public class Sepeda {
 
     protected Integer roda = 2;
+    private Long speed = 20l;
+
+    public Long getKecepatan(){
+        return this.speed;
+    }
+
+    public Long getKecepatan(Long tambahSpeed){
+        return this.speed + tambahSpeed;
+    }
+
 }
