fc1cbe01b5b5d4bee74f7fa1bf589674fff0c8c6
dimasm93 <software.dimas_m@icloud.com>
2019-12-17 23:25:57 +0700

    arrays part 2

--- src/main/java/com/maryanto/dimas/bootcamp/arrays/DataArrays.java
+++ src/main/java/com/maryanto/dimas/bootcamp/arrays/DataArrays.java
@@ -3,11 +3,15 @@ package com.maryanto.dimas.bootcamp.arrays;
 public class DataArrays {
 
     public static void main(String[] args) {
-        Integer[] values = {1, 2, 3, 4, 5, 10, 50};
-        for(int i = 0; i < values.length; i++){
-            System.out.println(
-                String.format("Nilai dari index ke %d adalah %d", i, values[i])
-            );
+        // Integer[] values = {1, 2, 3, 4, 5, 10, 50};
+
+        Integer[] values = new Integer[10];
+        values[0] = 1;
+        values[1] = 2;
+        values[5] = 3;
+        values[9] = 6;
+        for (int i = 0; i < values.length; i++) {
+            System.out.println(String.format("Nilai dari index ke %d adalah %d", i, values[i]));
         }
     }
 }
